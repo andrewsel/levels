b0770deabc50efa4d7e393a591b02f762824727b
Andrew Sellen <andrewmsellen@gmail.com>
2021-07-15 09:18:31 +1000

    feat: image selection and display

--- components/Food.tsx
+++ components/Food.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 import {colour, fontSize, spacing, radius} from '../styles/styles';
-import Icon from 'react-native-vector-icons/FontAwesome';
+// import Icon from 'react-native-vector-icons/FontAwesome';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import RNFS from 'react-native-fs';
 
 const Food = () => {
   const [response, setResponse] = React.useState<any>(null);
+  const [image, setImage] = React.useState(
+    'iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+  );
 
   const handleCameraPress = () => {
     launchCamera(
@@ -30,10 +34,25 @@ const Food = () => {
     console.log(response);
   };
 
+  const handleBase64 = async () => {
+    const path: string = response.assets[0].uri;
+    console.log('PATH: ' + path);
+    // const resizedImageUrl = await ImageResizer.createResizedImage(path, 200, 80, 'PNG', 80, 0, RNFS.DocumentDirectoryPath);
+    const base64 = await RNFS.readFile(path, 'base64');
+    setImage(base64);
+    // console.log('BASE64: ' + base64);
+  };
+
   return (
     <View style={s.foodContainer}>
       <View style={s.imageContainer}>
-        <Icon name="camera" size={40} color={colour.grey400} />
+        {/* <Icon name="camera" size={40} color={colour.grey400} /> */}
+        <Image
+          style={s.image}
+          source={{
+            uri: 'data:image/png;base64,' + image,
+          }}
+        />
       </View>
       <View>
         {/* <Text style={s.foodTitle}>Title</Text>
@@ -44,7 +63,10 @@ const Food = () => {
         <Text style={s.foodDescription} onPress={handleLibraryPress}>
           Photo Lib
         </Text>
-        <Text style={s.foodDescription}>Res: {JSON.stringify(response)}</Text>
+        <Text style={s.foodDescription} onPress={handleBase64}>
+          Base64
+        </Text>
+        {/* <Text style={s.foodDescription}>Res: {JSON.stringify(response)}</Text> */}
       </View>
     </View>
   );
@@ -73,11 +95,16 @@ const s = StyleSheet.create({
     backgroundColor: colour.grey900,
     width: 100,
     height: 100,
-    borderRadius: radius.round,
     marginRight: spacing * 2,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover',
+    borderRadius: radius.round,
   },
 });
 
