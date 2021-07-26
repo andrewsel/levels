b636e97c953b46746df00ef5a28fed54cfb02336
Andrew Sellen <andrewmsellen@gmail.com>
2021-07-26 14:41:05 +1000

    feat: menu design

--- components/Menu.js
+++ components/Menu.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
-import {colour} from '../styles/styles';
+import {colour, spacing} from '../styles/styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Toggle from './Toggle';
 
 const Menu = ({setScreen}) => {
   return (
     <View style={s.screen}>
-      <Text style={s.text}>MENU</Text>
       <TouchableOpacity onPress={() => setScreen('MAIN')}>
-        <Text style={s.text}>Close Menu</Text>
+        <Icon
+          name="chevron-left"
+          size={20}
+          color={colour.grey300}
+          onPress={() => setScreen('MAIN')}
+        />
       </TouchableOpacity>
+      <View style={s.avatarContainer}>
+        <View style={s.avatar}>
+          <Text style={s.avatarText}>CS</Text>
+        </View>
+        <Text style={s.text}>Chloe Sellen</Text>
+      </View>
+      <View style={s.row}>
+        <Text style={s.text}>Connect to health</Text>
+        <Toggle />
+      </View>
+      <View style={s.row}>
+        <Text style={s.text}>Insulin Types</Text>
+        <Icon
+          name="chevron-right"
+          size={20}
+          color={colour.grey300}
+          onPress={() => setScreen('MAIN')}
+        />
+      </View>
+      <View style={s.row}>
+        <Text style={s.text}>Tags</Text>
+        <Icon
+          name="chevron-right"
+          size={20}
+          color={colour.grey300}
+          onPress={() => setScreen('MAIN')}
+        />
+      </View>
+      <View style={[s.row, s.lastRow]}>
+        <Text style={[s.text, s.signOutText]}>Sign out</Text>
+      </View>
     </View>
   );
 };
@@ -17,11 +54,48 @@ const s = StyleSheet.create({
   screen: {
     backgroundColor: colour.grey900,
     flex: 1,
+    paddingHorizontal: spacing,
+    paddingVertical: 70,
   },
   text: {
     fontSize: 16,
     marginVertical: 20,
     color: colour.smoke,
+  },
+  avatarContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  avatar: {
+    height: 80,
+    width: 80,
+    backgroundColor: colour.pink,
+    borderRadius: 60,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatarText: {
+    fontSize: 32,
+    color: colour.grey900,
+    fontWeight: 'bold',
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderBottomColor: colour.smoke,
+    borderBottomWidth: 1,
+    paddingVertical: spacing,
+  },
+  lastRow: {
+    borderBottomWidth: 0,
+  },
+  signOutText: {
+    color: colour.red,
   },
 });
 
---
+++ components/Toggle.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, Pressable} from 'react-native';
+import {colour} from '../styles/styles';
+
+const Toggle = ({onPress}) => {
+  const [on, setOn] = useState(false);
+
+  return (
+    <Pressable
+      style={[s.container, on ? s.containerOn : s.containerOff]}
+      onPress={() => setOn(!on)}>
+      <View style={[s.circle, on ? s.circleOn : s.circleOff]} />
+    </Pressable>
+  );
+};
+
+const height = 24;
+const padding = 3;
+
+const s = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: colour.grey400,
+    borderRadius: 40,
+    height: height,
+    width: 46,
+
+    borderWidth: 1,
+  },
+  containerOn: {
+    justifyContent: 'flex-end',
+    borderColor: colour.green,
+  },
+  containerOff: {
+    justifyContent: 'flex-start',
+    borderColor: colour.grey200,
+  },
+  circle: {
+    height: height - padding * 2,
+    width: height - padding * 2,
+    borderRadius: 40,
+    marginHorizontal: padding,
+  },
+  circleOn: {
+    backgroundColor: colour.green,
+  },
+  circleOff: {
+    backgroundColor: colour.grey200,
+  },
+});
+
+export default Toggle;
